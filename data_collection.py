464520cb467b4cc288b0b1f369f7cfa225ad6dda
anil <anilkumarreddy3418@gmail.com>
2024-01-10 13:42:52 +0530

    collaborative based recommendation implemented

--- data_collection.py
+++ data_collection.py
@@ -1,53 +1,57 @@
-# importing libraries
 import pandas as pd
 import numpy as np
+from sklearn.metrics.pairwise import pairwise_distances 
+from io import StringIO
 
-# pass in column names for each CSV as the column name is not given in the file and read them using pandas.
-# You can check the column names from the readme file
-
-# reading users file:
 u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
 users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,encoding='latin-1')
 
-# reading ratings file:
 r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-ratings=pd.read_csv('ml-100k/u.data',sep='|',names=r_cols)
-# reading items file:
-i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+ratings=pd.read_csv('ml-100k/u.data',sep='\t',names=r_cols,encoding='latin-1')
+
+i_cols = ['movie_id', 'movie_title' ,'release_date','video_release_date', 'IMDb_URL', 'unknown', 'Action', 'Adventure',
 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
-items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,
-encoding='latin-1')
+items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,encoding='latin-1')
 
-# After loading the dataset, we should look at the content of each file (users, ratings, items).
-
-# Looking at the user file
-print("\nUser Data :")
 print("shape : ", users.shape)
 print(users.head())
 
-# We have 943 users in the dataset and each user has 5 features, i.e. user_ID, age, sex, occupation and zip_code. Now let’s look at the ratings file.
-
-# Ratings Data
-print("\nRatings Data :")
 print("shape : ", ratings.shape)
 print(ratings.head())
 
-# We have 100k ratings for different user and movie combinations. Now finally examine the items file.
-
-# Item Data
-print("\nItem Data :")
 print("shape : ", items.shape)
 print(items.head())
 
+# Collaborative Recommendation Model
+# find no of users
+n_users=users.user_id.unique().shape[0]
+n_items=items.movie_id.unique().shape[0]
+print(n_users,n_items,"no of users >>>>>>>>>>>>>>>>>")
+
+data_matrix = np.zeros((n_users, n_items))
+for line in ratings.itertuples():
+    data_matrix[line[1]-1, line[2]-1] = line[3]
+
+user_similarity = pairwise_distances(data_matrix, metric='cosine')
+item_similarity = pairwise_distances(data_matrix.T, metric='cosine')
+
+print("user similarity is ",user_similarity)
+def predict(ratings, similarity, type='user'):
+    if type == 'user':
+        mean_user_rating = ratings.mean(axis=1)
+        #We use np.newaxis so that mean_user_rating has same format as ratings
+        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif type == 'item':
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+    return pred
+
+user_prediction = predict(data_matrix, user_similarity, type='user')
+item_prediction = predict(data_matrix, item_similarity, type='item')
+
+print("user pred is ",user_prediction,item_prediction)
 # r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 # ratings_train = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
 # ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
 # print(ratings_train.shape, ratings_test.shape)
-
-# Collaborative Recommendation Model
-# find no of users
-no_of_users=users.user_id.unique().shape[0]
-no_of_movies=items.movie_id.unique().shape[0]
-print(no_of_users,no_of_movies,"no of users >>>>>>>>>>>>>>>>>")
-
